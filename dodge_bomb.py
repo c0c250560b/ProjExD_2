a2c3c5e92d52e3b01c931fbf1a3ea721bcfa0c27
kimura <c0c250560b@edu.ed.ac.jp>
2026-04-21 17:12:01 +0900

    演習問題２途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,18 +6,30 @@ import time
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-def gameover(screen: pg.Surface) -> None:
-    gm_img = pg.Surface((WIDTH, HEIGHT))
-    gm_img.set_alpha(200)
-    screen.blit(gm_img,screen.get_rect())
-    fonto = pg.font.Font(None, 80)
-    txt = fonto.render("GAME OVER", True, (255, 255, 255))
-    screen.blit(txt, [300,200])
-    kk_img = pg.image.load("fig/8.png").convert_alpha()
-    screen.blit(kk_img, [200,200])
-    screen.blit(kk_img, [700,200])
-    pg.display.update()
-    time.sleep(5)
+#　演習課題１
+def gameover(screen: pg.Surface) -> None:  # ゲームオーバーの画面を表示する関数
+    gm_img = pg.Surface((WIDTH, HEIGHT))  # ゲームオーバーの背景を作成
+    gm_img.set_alpha(200)  # 透明度を設定
+    screen.blit(gm_img,screen.get_rect())  # ゲームオーバーの背景を画面に描く
+    fonto = pg.font.Font(None, 80)  # フォントを作成
+    txt = fonto.render("GAME OVER", True, (255, 255, 255))  # ゲームオーバーの文字を描く
+    screen.blit(txt, [300,200])  # ゲームオーバーの文字を画面に描く
+    kk_img = pg.image.load("fig/8.png").convert_alpha()  # こうかとんの画像を読み込む
+    screen.blit(kk_img, [200,200])  # こうかとんの画像を画面に描く
+    screen.blit(kk_img, [700,200])  # こうかとんの画像を画面に描く
+    pg.display.update()  # ゲームオーバーの画面を更新
+    time.sleep(5)  # ゲームオーバーの画面を5秒間表示する
+
+# 演習課題２
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    bb_imgs = []  
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r), pg.SRCALPHA)  
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_imgs.append(bb_img)
+    bb_accs = [a for a in range(1, 11)]
+
+    return bb_imgs, bb_accs
 
 def check_bound(rct : pg.Rect) -> tuple[bool, bool]:
     """
@@ -25,9 +37,9 @@ def check_bound(rct : pg.Rect) -> tuple[bool, bool]:
     戻り値：横方向、縦方向判定効果（True: 画面内　False: 画面外）
     """
     yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
         yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
         tate = False
     return yoko, tate
 
@@ -36,25 +48,35 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
-    bb_img = pg.Surface((20, 20))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_img.set_colorkey((0, 0, 0))
-    bb_rct = bb_img.get_rect()
-    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    vx, vy = +5, +5
+    bb_img = pg.Surface((20, 20))  # 爆弾の画像を作成
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 爆弾を赤い円で描く 
+    bb_img.set_colorkey((0, 0, 0))  # 爆弾の黒い部分を透過する
+    bb_rct = bb_img.get_rect()  # 爆弾のRectを取得
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)  # 爆弾の位置をランダムに設定
+    vx, vy = +5, +5  # 爆弾の速度を設定
 
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
+
+    bb_imgs, bb_accs = init_bb_imgs()
+    avx = vx*bb_accs[min(tmr//500,9)]
+    avy = vy*bb_accs[min(tmr//500,9)]
+    bb_img = bb_imgs[min(tmr//500,9)]
+    bb_rct.move_ip(avx, avy)
+    bb_rct.width = bb_img.get_rect().width
+    bb_rct.height = bb_img.get_rect().height
+    
     while True:
+        
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        if kk_rct.colliderect(bb_rct):
+        if kk_rct.colliderect(bb_rct):  # こうかとんと爆弾が重なったら
             gameover(screen)
             print("ゲームオーバー")
-            return
+            return  # ゲームオーバーになったらmain関数を抜ける
         screen.blit(bg_img, [0, 0]) 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -77,18 +99,19 @@ def main():
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) !=(True, True):
+        if check_bound(kk_rct) !=(True, True):  # こうかとんが画面外に出たら
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
+
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+        bb_rct.move_ip(vx, vy)  # 爆弾を動かす
         yoko, tate = check_bound(bb_rct)
-        if not yoko:
+        if not yoko:  # 横方向の判定
             vx *= -1
-        if not tate:
+        if not tate:  # 縦方向の判定
             vy *= -1
 
-        screen.blit(bb_img, bb_rct)
+        screen.blit(bb_img, bb_rct)  # 爆弾を画面に描く
         pg.display.update()
         tmr += 1
         clock.tick(50)
